b05bc632655cefa1cef3a105c2f84106ff1b303b
Clider <160562510+KodepeDev@users.noreply.github.com>
2025-03-03 10:17:35 -0500

    configuracion de perfil de usuario

--- config/adminlte.php
+++ config/adminlte.php
@@ -138,7 +138,7 @@ return [
     'usermenu_header_class' => 'bg-primary',
     'usermenu_image' => true,
     'usermenu_desc' => true,
-    'usermenu_profile_url' => false,
+    'usermenu_profile_url' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -263,7 +263,7 @@ return [
     'register_url' => false,
     'password_reset_url' => false, //'password/reset'
     'password_email_url' => 'password/email',
-    'profile_url' => true,
+    'profile_url' => false,
     'disable_darkmode_routes' => false,
 
     /*
