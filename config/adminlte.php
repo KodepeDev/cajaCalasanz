400f32ee2b200c72a17050a58f3fc9286a513920
Clider <160562510+KodepeDev@users.noreply.github.com>
2025-03-03 09:52:40 -0500

    create account none

--- config/adminlte.php
+++ config/adminlte.php
@@ -260,8 +260,8 @@ return [
     'dashboard_url' => 'home',
     'logout_url' => 'logout',
     'login_url' => 'login',
-    'register_url' => 'register',
-    'password_reset_url' => 'password/reset',
+    'register_url' => false,
+    'password_reset_url' => false, //'password/reset'
     'password_email_url' => 'password/email',
     'profile_url' => true,
     'disable_darkmode_routes' => false,
