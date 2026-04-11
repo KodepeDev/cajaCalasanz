3dabb92a136b9b898109eafd21095e2b280ea5f9
Clider <78662332+CLIDR@users.noreply.github.com>
2026-04-11 13:06:50 -0500

    update dashboard details

--- app/Http/Livewire/Movimientos/Components/GraficaMovimiento.php
+++ app/Http/Livewire/Movimientos/Components/GraficaMovimiento.php
@@ -6,28 +6,29 @@ use App\Charts\MonthlySummariesChart;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 use ArielMejiaDev\LarapexCharts\LarapexChart;
+use App\Models\SchoolYear;
 
 class GraficaMovimiento extends Component
 {
-
     public $years = [];
     public $selectedYear;
 
     public function mount()
     {
-        $this->years = DB::table('summaries')
-            ->select(DB::raw('YEAR(date) as year'))
-            ->groupBy('year')
-            ->pluck('year');
+        $this->years = DB::table("summaries")
+            ->select(DB::raw("YEAR(date) as year"))
+            ->groupBy("year")
+            ->pluck("year");
 
-        $this->selectedYear = date('Y');
+        $this->selectedYear = SchoolYear::find(
+            session("current_school_year_id"),
+        )->year;
     }
     public function render(MonthlySummariesChart $chart)
     {
-
-        return view('livewire.movimientos.components.grafica-movimiento', [
-            'chartData' => $chart->build(),
-            'years' => $this->years,
+        return view("livewire.movimientos.components.grafica-movimiento", [
+            "chartData" => $chart->build(),
+            "years" => $this->years,
         ]);
     }
 }
